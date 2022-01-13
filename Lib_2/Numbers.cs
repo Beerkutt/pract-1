f71e7565d0dde40df155702fb356ba91912a7ec7
Beerkut <Beerkut@DESKTOP-2NOOI69>
2022-01-13 06:42:44 +0300

    Добавьте файлы проекта.

---
+++ Lib_2/Numbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib_2
+{
+    public static class Numbers
+    {
+        static Random rnd = new Random();
+        /// <summary>
+        ///  Находит разность сгенерированных чисел до тех пор, пока разность < числа К.
+        /// </summary>
+        /// <param name="k">Число К</param>
+        /// <returns>Сгенерированные числа, значение разности, и количество сгенерированных чисел.</returns>
+        public static (string, string, int) Difference(int k)
+        {
+            string numbers = "";
+            int difference = 0;
+            int count =0 ;
+            while (k < difference)
+            {
+                int currentvalue = rnd.Next(2, 10);
+                difference -= currentvalue;
+                numbers += currentvalue.ToString() + " ";
+                count++;
+            }
+            return (numbers, difference.ToString(), count);
+        }
+    }
+}
---
+++ pract 1/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lib_2;
+
+namespace pract_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void exit(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void info(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("ИСП - 34, Голота, Вариант 2. Вычислить разницу целых случайных чисел, распределенных в диапазоне от 2 до 10, пока эта разница не станет меньше некоторого числа K (K<0). Вывести на экран сгенерированные числа, значение суммы, и количество сгенерированных чисел.");
+        }
+
+        private void find(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (int.Parse(tbk.Text) < 0)
+                {
+                    var rez = Numbers.Difference(int.Parse(tbk.Text));
+                    tbnumbers.Text = rez.Item1;
+                    tbdifference.Text = rez.Item2;
+                    tbcount.Text = rez.Item3.ToString();
+                } else MessageBox.Show("Введите отрицательное число!", "Внимание!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка {ex.Data.ToString()}", "Внимание!");
+            }
+            
+        }
+    }
+}
